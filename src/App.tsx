6e4e04021ac061a682e0ee0765b246bc334cea84
Dmitry <dmitry@example.com>
2026-07-26 12:21:59 +0300

    Добавлена кнопка Назад на страницу заявки

--- src/App.tsx
+++ src/App.tsx
@@ -766,13 +766,40 @@ const ambassadorCode =
   }
 
   if (
-    !canSubmitMany &&
-    existingApplication &&
-    existingApplication.status !== "Отклонена"
-  ) {
-    return (
-      <div className="page">
-        <h1>📝 Заявка участницы</h1>
+  !canSubmitMany &&
+  existingApplication &&
+  existingApplication.status !== "Отклонена"
+) {
+  return (
+    <div className="page">
+
+      <button
+        onClick={() => window.history.back()}
+        style={{
+          width: "100%",
+          padding: "14px",
+          marginBottom: "0px",
+          borderRadius: "16px",
+          border: "1px solid #FFD54A",
+          background: "linear-gradient(180deg,#FFE88A,#D6A800)",
+          color: "#111",
+          fontWeight: "700",
+          fontSize: "24px",
+          cursor: "pointer",
+          boxShadow: "0 0 18px rgba(255,215,0,.35)",
+        }}
+      >
+        ← Назад
+      </button>
+
+      <h1
+        style={{
+          marginTop: "8px",
+          marginBottom: "12px",
+        }}
+      >
+        📝 Заявка участницы
+      </h1>
 
         <div className="card">
           <img
